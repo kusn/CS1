0050a06830bbc8fd7f93dc7f938da186a4056c7b
Kusn <serjikf@yandex.ru>
2020-11-27 15:36:47 +0300

    Начал  ДЗ_2

---
+++ Home_work_2/Minimum_of_three/Program.cs
@@ -0,0 +1,40 @@
+﻿//Кудряшов Сергей
+
+// Написать метод, возвращающий минимальное из трёх чисел.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimum_of_three
+{
+    class Program
+    {
+        static double Min_of_three(double a, double b, double c)
+        {
+            double min = new double();
+            if (a <= b)
+                min = a;
+            else
+                min = b;
+            if (c < min)
+                min = c;            
+            return min;
+        }
+
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.WriteLine("Вычислим минимальное из трех чисел.");
+            Console.WriteLine("Введите первое число");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите третье число число");
+            c = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"{Min_of_three(a, b, c)} - минимальное из введенных чисел");
+        }
+    }
+}
---
+++ Home_work_2/Sum_of_odd_positive_numbers/Program.cs
@@ -0,0 +1,39 @@
+﻿// Кудряшов Сергей
+
+// С клавиатуры вводятся числа, пока не будет введен 0.
+// Подсчитать сумму всех нечетных положительных чисел.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_odd_positive_numbers
+{
+    class Program
+    {
+        static bool Even(int a)
+        {
+            return a % 2 == 0;
+        }
+
+        static void Main(string[] args)
+        {
+            int number = new int();
+            int sum = 0;
+
+            Console.WriteLine("С клавиатуры вводятся числа, пока не будет введен 0. " +
+                "Подсчитывается сумма всех нечетных положительных чисел.");
+            do
+            {
+                Console.WriteLine("Введите число");
+                number = int.Parse(Console.ReadLine());
+                if (!Even(number) && number >= 0)
+                    sum = sum + number;
+            }
+            while (number != 0);
+            Console.WriteLine("Сумма введеных чисел = {0}", sum);
+        }
+    }
+}
