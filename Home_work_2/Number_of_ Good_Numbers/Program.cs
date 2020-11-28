d204691c7457856dfc9e14f4b6d34538851ddeef
kusn <serjikf@yandex.ru>
2020-11-28 20:35:03 +0300

    Добавлена 7 задача

--- Home_work_2/Number_of_ Good_Numbers/Program.cs
+++ Home_work_2/Number_of_ Good_Numbers/Program.cs
@@ -29,7 +29,7 @@ namespace Number_of__Good_Numbers
         {
             int numbers = 0;            
 
-            for (int i = 1; i <= 1000000; i++)
+            for (int i = 1; i <= 1000000000; i++)
             {
                 int sum = 0;
                 sum = SumOfDigits(i);
@@ -45,11 +45,12 @@ namespace Number_of__Good_Numbers
         static void Main(string[] args)
         {
             int k = 0;
+            Console.WriteLine("Происходит подсчет...");
             var startTime = System.Diagnostics.Stopwatch.StartNew();
             k = NumberOfGoodNumbers();
             startTime.Stop();
             var resultTime = startTime.Elapsed;
-            Console.WriteLine($"Количество хороших чисел от 1 до 1000000 равно {k}");
+            Console.WriteLine($"Количество хороших чисел от 1 до 1000000000 равно {k}");
             Console.WriteLine($"Для вычисления понадобилось {resultTime.TotalMilliseconds} мс");
         }
     }
