fa95e316d50d2c523f000c1a5850742169827746
kusn <serjikf@yandex.ru>
2020-12-03 21:43:46 +0300

    Начата вторая задача

---
+++ Home_work_3/Sum_of_odd_positive_numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿// Кудряшов Сергей
+
+// С клавиатуры вводятся числа, пока не будет введён 0 (каждое число в новой строке).
+// Требуется подсчитать сумму всех нечётных положительных чисел. Сами числа и сумму вывести на экран,
+// используя tryParse.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_odd_positive_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+
+            Console.WriteLine("Подсчёт суммы всех нечётных положительных чисел");
+            Console.WriteLine("Введите любое целое число. Для выхода нажмите 0");
+            do
+            {
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            while (n != 0);
+        }
+    }
+}
