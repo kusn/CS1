0dc84772a3196697807a0e08761b7df456fb7bd1
kusn <serjikf@yandex.ru>
2020-12-10 20:58:46 +0300

    Начал ДЗ5

---
+++ Home_work_5/CorrectLogin/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorrectLogin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
