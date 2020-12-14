6ee1ef8168d5a9acfa7d5201885190921b1b308f
Kusn <serjikf@yandex.ru>
2020-12-14 16:39:47 +0300

    Начал задачу 2

---
+++ Home_work_6/MinOfFunctions/Program.cs
@@ -0,0 +1,65 @@
+﻿// Кудряшов Сергей
+
+// Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию
+// в виде делегата. 
+// а) Сделать меню с различными функциями и представить пользователю выбор, для какой функции
+// и на каком отрезке находить минимум. Использовать массив (или список) делегатов, в котором
+// хранятся различные функции.
+// б) *Переделать функцию Load, чтобы она возвращала массив считанных значений. Пусть она
+// возвращает минимум через параметр (с использованием модификатора out). 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MinOfFunctions
+{
+    public delegate double Fun(double x);
+
+    class Program
+    {
+        public static double F(double x)
+        {
+            return x * x - 50 * x + 10;
+        }
+        public static void SaveFunc(Fun FF, string fileName, double a, double b, double step)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(F(x));
+                x += step;// x=x+step;
+            }
+            bw.Close();
+            fs.Close();
+        }
+        public static double Load(string fileName)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            double min = double.MaxValue;
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d = bw.ReadDouble();
+                if (d < min) min = d;
+            }
+            bw.Close();
+            fs.Close();
+            return min;
+        }
+
+        static void Main(string[] args)
+        {
+            SaveFunc("data.bin", -100, 100, 0.5);
+            Console.WriteLine(Load("data.bin"));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Home_work_6/MinOfFunctions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("MinOfFunctions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MinOfFunctions")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("85f92335-4198-4c7c-954c-617205334804")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
