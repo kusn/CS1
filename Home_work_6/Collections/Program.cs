80138e44bfc8cf73685a0c467953add171399b05
kusn <serjikf@yandex.ru>
2020-12-16 12:23:02 +0300

    Выполнил 3 задачу

--- Home_work_6/Collections/Program.cs
+++ Home_work_6/Collections/Program.cs
@@ -39,28 +39,38 @@ namespace Collections
             this.group = group;
             this.city = city;
         }
+
+        public override string ToString()
+        {
+            return firstName + ";" + lastName + ";" + university + ";" + faculty + ";" + department + ";" + age + ";" + course + ";" + group + ";" + city;
+        }
     }
 
 
     class Program
     {
-        static int MyDelegat_firstName(Student st1, Student st2)                  // Создаем метод для сравнения для экземпляров
+        /*static int MyDelegat_firstName(Student st1, Student st2)        // Создаем метод для сравнения для экземпляров
         {
 
             return String.Compare(st1.firstName, st2.firstName);        // Сравниваем две строки
-        }
+        }*/
 
-        static int MyDelegat_course(Student st1, Student st2)                  // Создаем метод для сравнения для экземпляров
+        static int MyDelegat_course(Student st1, Student st2)           // Создаем метод для сравнения для экземпляров
         {
             if (st1.course > st2.course) return 1;
-            else if (st1.course > st2.course) return -1;
-            else return 0;
+            else if (st1.course < st2.course) return -1;
+            else
+            {
+                if (st1.age > st2.age) return 1;
+                else if (st1.age < st2.age) return -1;
+                else return 0;
+            }
         }
 
-        static int MyDelegat_age(Student st1, Student st2)                  // Создаем метод для сравнения для экземпляров
+        static int MyDelegat_age(Student st1, Student st2)              // Создаем метод для сравнения для экземпляров
         {
             if (st1.age > st2.age) return 1;
-            else if (st1.age > st2.age) return -1;
+            else if (st1.age < st2.age) return -1;
             else return 0;
         }
 
@@ -72,22 +82,23 @@ namespace Collections
             Dictionary<int, int> dict = new Dictionary<int, int>();     // Создаем кол-во студентов от 18 до 20 лет на каждом курсе
             List<Student> list = new List<Student>();                   // Создаем список студентов
             DateTime dt = DateTime.Now;
-            StreamReader sr = new StreamReader("..//..//students.csv", Encoding.GetEncoding(1251));
+            StreamReader sr = new StreamReader("..//..//students_4.csv", Encoding.GetEncoding(1251));
             int i = 0;
             while (!sr.EndOfStream)
             {
                 try
                 {
-                    string[] s = sr.ReadLine().Split(';');
                     // Добавляем в список новый экземпляр класса Student
+                    string[] s = sr.ReadLine().Split(';');
                     list.Add(new Student(s[0], s[1], s[2], s[3], s[4], int.Parse(s[5]), int.Parse(s[6]), int.Parse(s[7]), s[8]));
-                    
-                    // Одновременно подсчитываем количество бакалавров и магистров
+
+                    // Одновременно подсчитываем количество студентов на 5 и 6 курсе
                     if (int.Parse(s[6]) == 5) count_stud_5++;
                     else if (int.Parse(s[6]) == 6) count_stud_6++;
-
+                    
+                    // Считаем кол-во студентов от 18 до 20 лет на каждом курсе
                     if (int.Parse(s[5]) >= 18 && int.Parse(s[5]) <= 20)
-                        if (dict.Count != 0 && dict.ContainsValue(int.Parse(s[6])))
+                        if (dict.Count != 0 && dict.ContainsKey(int.Parse(s[6])))
                         {
                             dict[int.Parse(s[6])]++;
                         }
@@ -116,13 +127,27 @@ namespace Collections
             foreach (var v in dict)
                 Console.WriteLine(v.Key + " - " + v.Value);
 
+            StreamWriter sw1 = new StreamWriter("..//..//student_sort_age.csv");
             Console.WriteLine(System.Environment.NewLine + "Сортировка студентов по возрасту:");
             list.Sort(new Comparison<Student>(MyDelegat_age));
-            foreach (var v in list) Console.WriteLine(v.firstName + "-" + v.age);
+            //foreach (var v in list) Console.WriteLine(v.firstName + "-" + v.age);
+            foreach (var v in list)
+            {
+                sw1.WriteLine(v);
+                //Console.WriteLine(v.firstName + "-" + v.age);                
+            }
+            sw1.Close();
 
+            StreamWriter sw2 = new StreamWriter("..//..//student_sort_course_age.csv");
             Console.WriteLine(System.Environment.NewLine + "Сортировка студентов по курсу и возрасту:");
             list.Sort(new Comparison<Student>(MyDelegat_course));
-            foreach (var v in list) Console.WriteLine(v.firstName);
+            //foreach (var v in list) Console.WriteLine(v.firstName + " - " + v.course + " - " + v.age);
+            foreach (var v in list)
+            {
+                sw2.WriteLine(v);
+                //Console.WriteLine(v.firstName + "-" + v.age);               
+            }
+            sw2.Close();
 
             Console.WriteLine(DateTime.Now - dt);
             Console.ReadKey();
