4787be9d65091a86fdfe9a30fab575d9539e9c94
Kusn <serjikf@yandex.ru>
2020-12-19 11:56:57 +0300

    Начал 2 задачу

---
+++ Home_work_7/GuessNumber/Form1.cs
@@ -0,0 +1,26 @@
+﻿// Кудряшов Сегей
+
+// Используя Windows Forms, разработать игру «Угадай число».
+// Компьютер загадывает число от 1 до 100, а человек пытается его угадать за минимальное число попыток.
+// Для ввода данных от человека используется элемент TextBox.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuessNumber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
