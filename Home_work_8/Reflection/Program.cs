476a74a0709f0418403f0054b5784e89f4aadfb9
Kusn <serjikf@yandex.ru>
2020-12-21 08:11:44 +0300

    Выполнил 1 задачу

---
+++ Home_work_8/Reflection/Program.cs
@@ -0,0 +1,48 @@
+﻿// Кудряшов Сергей
+
+// С помощью рефлексии выведите все свойства структуры DateTime
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        static PropertyInfo GetPropertyInfo(object obj, string str)
+        {
+            return obj.GetType().GetProperty(str);
+        }
+
+
+        static void Main(string[] args)
+        {
+            Type type = typeof(DateTime);
+            DateTime dateTime = new DateTime();
+            
+            Console.WriteLine(type);
+            Console.WriteLine(type.GetType());
+            
+            Console.WriteLine(GetPropertyInfo(dateTime, "DayOfWeek").Name);
+            Console.WriteLine(GetPropertyInfo(dateTime, "DayOfWeek").CanRead);
+            Console.WriteLine(GetPropertyInfo(dateTime, "DayOfWeek").CanWrite);
+            Console.WriteLine(GetPropertyInfo(dateTime, "DayOfWeek").GetValue(dateTime, null));
+            Console.WriteLine(GetPropertyInfo(dateTime, "DayOfWeek").Attributes);
+            Console.WriteLine(GetPropertyInfo(dateTime, "DayOfWeek").CustomAttributes);
+            Console.WriteLine(GetPropertyInfo(dateTime, "DayOfWeek").GetMethod);
+            Console.WriteLine(GetPropertyInfo(dateTime, "DayOfWeek").IsSpecialName);
+            Console.WriteLine(GetPropertyInfo(dateTime, "DayOfWeek").MemberType);
+            Console.WriteLine(GetPropertyInfo(dateTime, "DayOfWeek").MetadataToken);
+            Console.WriteLine(GetPropertyInfo(dateTime, "DayOfWeek").Module);
+            Console.WriteLine(GetPropertyInfo(dateTime, "DayOfWeek").PropertyType);
+            Console.WriteLine(GetPropertyInfo(dateTime, "DayOfWeek").ReflectedType);
+            Console.WriteLine(GetPropertyInfo(dateTime, "DayOfWeek").SetMethod);
+
+            Console.ReadKey();
+        }
+    }
+}
