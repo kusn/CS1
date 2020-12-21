c31f793d2b5ae1458a9d5b385349cb5d3c9c7b7d
kusn <serjikf@yandex.ru>
2020-12-21 21:59:29 +0300

    Начал 4 задачу

---
+++ Home_work_8/Birthdays/DateStore.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Birthdays
+{
+    class DateStore
+    {
+        string fileName;
+        List<Birthday> list;
+        
+        public string FileName
+        {
+            set { fileName = value; }
+        }
+
+        public DateStore(string filename)
+        {
+            this.fileName = fileName;
+            list = new List<Birthday>();
+        }
+
+        public void Add(string text, DateTime date)
+        {
+            list.Add(new Birthday(text, date));
+        }
+
+        public void Remove(int index)
+        {
+            if (list != null && index < list.Count && index > 0)
+                list.RemoveAt(index);
+        }
+
+        public Birthday this[int index]
+        {
+            get { return list[index]; }
+        }
+
+        public void Save()
+        {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<Birthday>));
+            Stream fStream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            xmlFormat.Serialize(fStream, list);
+            fStream.Close();
+        }
+
+        public void SaveAs(string fname)
+        {
+            XmlSerializer xmlFormat = new XmlSerializer(typeof(List<Birthday>));
+            Stream fStream = new FileStream(fname, FileMode.Create, FileAccess.Write);
+            xmlFormat.Serialize(fStream, list);
+            fStream.Close();
+        }
+
+        public void Load()
+        {
+            XmlSerializer xmlformat = new XmlSerializer(typeof(List<Birthday>));
+            Stream fStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            list = (List<Birthday>)xmlformat.Deserialize(fStream);
+            fStream.Close();
+        }
+
+        public int Search(DateTime date)
+        {
+            int i = 0;
+            int n = 0;
+            foreach(var v in list)
+            {
+                if(v.date == date)
+                {
+                    n = i;
+                }
+                i++;
+            }
+            return i;
+        }
+
+        public int Count
+        {
+            get { return list.Count; }
+        }
+    }
+}
---
+++ Home_work_8/Birthdays/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Birthdays
+{
+    public partial class Form1 : Form
+    {
+
+        DateStore database;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                database = new DateStore(sfd.FileName);
+                database.Add("Дата создания", monthCalendar1.TodayDate);
+                database.Save();
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (database == null)
+            {
+                MessageBox.Show("Создайте новую базу данных", "Сообщение");
+                return;
+            }
+            database.Add((database.Count + 1).ToString(), monthCalendar1.SelectionRange.Start);
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (database == null) return;
+            database.Remove()
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void monthCalendar1_DateSelected(object sender, DateRangeEventArgs e)
+        {
+            textBox1.Text = database[database.Search(monthCalendar1.SelectionRange.Start)].text;
+        }
+    }
+}
